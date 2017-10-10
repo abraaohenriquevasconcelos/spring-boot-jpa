98782fb7f6d41211a7a6fc6a316f3beb03e216a0
abraaohenriquevasconcelos <henrique.vscncls@gmail.com>
2017-10-10 15:55:42 -0300

    ...

---
+++ src/main/java/spring/AprendendoSpringBootApplication.java
@@ -0,0 +1,12 @@
+package spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendendoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprendendoSpringBootApplication.class, args);
+	}
+}
